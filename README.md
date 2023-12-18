72ce9dcce4a53eca683252dcd7f18d2fcff458a9
0xFB499 <0xfb499@proton.me>
2023-12-18 22:56:32 +0100

    Uploaded project

---
+++ README.md
@@ -0,0 +1,19 @@
+# Diceware Passphrase Generator
+Pretty self-explainatory, this script just translate numbers to words from a words list file.
+
+This script **can** generate random numbers. But you should do it yourself with some dice :)
+
+More on [diceware](https://en.wikipedia.org/wiki/Diceware)
+
+--------
+## Usage
+```
+python main.py <path/to/wordlist-file> (-f <path/to/numbers-file> | -g <n. words to generate> | -i)
+```
+where:
+
+- a _wordlist_ is a file that contains some lines in the format ```<digits> <word>``` (every other text is ignore)
+- a _numberlist_ is a file that contains a number for every line
+- ```-i``` make input interactive: give any number and print the associated word
+- ```-g``` request _n_ (preferably > 7) numbers from [random.org](https://www.random.org) and translate into words
+- ```-f``` read numbers from file
---
+++ main.py
@@ -0,0 +1,103 @@
+import sys
+import argparse as arg
+import requests
+import re
+import signal
+
+DICE_ROLLS = 5
+
+def add(set: dict, num: str, word: str):
+    if len(num) == 1:
+        set[num] = word
+        return
+
+    if num[0] not in set:
+        set[num[0]] = {}
+    add(set[num[0]], num[1:], word)
+
+def search(set: dict, num: str):
+    if len(num) != DICE_ROLLS:
+        return None
+    return search_rec(set, num)
+
+def search_rec(set: dict, num: str):
+    if len(num) == 1:
+        return set[num]
+    if num[0] not in set:
+        return None
+    else:
+        return search_rec(set[num[0]], num[1:])
+
+def readNumbersFile(file) -> list:
+    res = list()
+    with file as f:
+        for line in f:
+            line = line.removesuffix('\n')
+            res.append(line)
+    return res
+
+def generateNumbers(dim : int) -> list:
+
+    r = requests.get("https://www.random.org/integers", {
+        'num': 5*dim,
+        'min': 1,
+        'max': 6,
+        'col': 5,
+        'base': 10,
+        'format': 'plain',
+        'rnd': 'new'})
+
+    if r.status_code != 200 :
+        print(r.text)
+        return []
+
+    return r.text.replace('\t', '').split('\n')[:-1]
+
+
+def main():
+    parser = arg.ArgumentParser()
+    parser.add_argument("wordlist", type=arg.FileType("r", encoding="utf_8"), help="wordlist file")
+    grp = parser.add_mutually_exclusive_group(required=True)
+    grp.add_argument("-i", action="store_true", help="interactive input")
+    grp.add_argument("-g", type=int, help="generate x words")
+    grp.add_argument("-f", type=arg.FileType("w", encoding="utf_8"), help="use file as input")
+    args = parser.parse_args()
+
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+    tree = {}
+    with args.wordlist as f:
+        r = re.compile(f"\d{ {DICE_ROLLS} } \w*")
+        for line in f:
+            if r.match(line) is not None:
+                add(tree, line[:DICE_ROLLS], line[DICE_ROLLS+1:-1])
+
+    print("Word tree generated.")
+
+    if args.f is not None:
+        for num in readNumbersFile(args.f):
+            print(search(tree, num))
+        print()
+        return
+
+    if args.g is not None :
+        for num in generateNumbers(args.g):
+            print(search(tree, num), end= " ")
+        print()
+        return
+
+    if args.i:
+        i = 1
+        while True:
+            line = input()
+            line = line.removesuffix('\n')
+            res = search(tree, line)
+            if res is not None:
+                print(f"{i}. {res}", end = "\n")
+                i+=1
+            else:
+                print("Not Found")
+
+if __name__ == "__main__":
+    main()
+
